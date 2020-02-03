00248dafcb55d3c90709d5a26879c621d55ac171
RJ Trujillo <certifiedblyndguy@gmail.com>
2020-02-02 21:03:27 -0700

    h932: Carbonize
    Signed-off-by: RJ Trujillo <certifiedblyndguy@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/lineage_h932.mk
+    $(LOCAL_DIR)/carbon_h932.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,3 @@
-add_lunch_combo lineage_h932-userdebug
+add_lunch_combo carbon_h932-eng
+add_lunch_combo carbon_h932-userdebug
+add_lunch_combo carbon_h932-user
